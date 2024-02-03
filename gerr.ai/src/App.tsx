440219ded207817be2919b46bc3d3931a437d040
Este-flan13 <eg7075@princeton.edu>
2024-02-03 17:23:23 -0500

    Info Section

--- gerr.ai/src/App.tsx
+++ gerr.ai/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import BackgroundWithText from './BackgroundWithText';
 import Extra from './extra';
 import CustomCanvasDraw from './CustomCanvasDraw';
+import About from './About';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <p>Let the Elections Begin</p>
       {/* Your app content */}
       { <CustomCanvasDraw />}
+      { <About /> }
     </div>
   );
 }
