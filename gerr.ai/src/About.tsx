b23d1677a3d3d3066a5a3daad56457beb916fd6f
Hang Pham <hangtpham04@gmail.com>
2024-02-04 09:25:10 -0500

    fix: whitespace issue

--- gerr.ai/src/About.tsx
+++ gerr.ai/src/About.tsx
@@ -5,25 +5,26 @@ import './About.css';
 interface AboutProps {
     title: string;
     info: string;
- }
- 
- const About: React.FC<AboutProps> = ({title, info }) => {
+}
 
-  return (
+const About: React.FC<AboutProps> = ({ title, info }) => {
 
-             <div style={
-                {
-                width: "1000px", /* Adjust the width as needed */
-                height: "400px", /* Adjust the height as needed */
+    return (
+
+        <div style={
+            {
+                width: "80%", /* Adjust the width as needed */
+                height: "675px", /* Adjust the height as needed */
                 backgroundColor: "#e0e0e0", /* Set the background color */
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
                 marginTop: "100px", /* Center the box horizontally */
                 marginBottom: "0px",
-                marginLeft: "420px",
+                marginLeft: "10%",
                 paddingLeft: "20px",
-                paddingTop: "0px",
+                paddingTop: "-70px",
+                paddingBottom: "60px",
                 // display: "flex",
                 // alignItems: "center",
                 border: "2px solid black",
@@ -37,14 +38,14 @@ interface AboutProps {
                 // backgroundColor: "#F4EBE6",  
                 // objectFit: "contain",
                 // boxSizing: "border-box",
-             }}>  
-             {/* <p> HI </p> */}
-                 <h1 className="About">{title}</h1>
-             {/* <p style={{}} className="About">{title}</p>     */}
-             <p className="About2">{info}</p>
-             </div>
-    
-  );
+            }}>
+            {/* <p> HI </p> */}
+            <h1 className="About">{title}</h1>
+            {/* <p style={{}} className="About">{title}</p>     */}
+            <p className="About2">{info}</p>
+        </div>
+
+    );
 }
 
 export default About;
