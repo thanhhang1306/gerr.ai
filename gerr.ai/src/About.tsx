c2d9c7d204d0f275129791f49039d701dbd679c9
Dylan Epstein-Gross <de7281@princeton.edu>
2024-02-04 02:40:28 -0500

    Changed small things

--- gerr.ai/src/About.tsx
+++ gerr.ai/src/About.tsx
@@ -11,19 +11,37 @@ interface AboutProps {
 
   return (
 
-             <div style={{
+             <div style={
+                {
+                width: "1000px", /* Adjust the width as needed */
+                height: "400px", /* Adjust the height as needed */
+                backgroundColor: "#e0e0e0", /* Set the background color */
                 display: "flex",
+                justifyContent: "center",
                 alignItems: "center",
+                marginTop: "100px", /* Center the box horizontally */
+                marginBottom: "0px",
+                marginLeft: "420px",
+                paddingLeft: "20px",
+                paddingTop: "0px",
+                // display: "flex",
+                // alignItems: "center",
                 border: "2px solid black",
-                padding: "50px",
-                maxWidth: "50%",
-                margin: "0 auto",     
-                backgroundColor: "#F4EBE6",  
-                objectFit: "contain",
-                boxSizing: "border-box",
+                // border: "2px solid black",
+                // marginTop: "200px",
+                // paddingTop: "0px",
+                // lineHeight: "1",
+                // maxWidth: "50%",
+                // marginLeft: "450px",
+                // justifyContent: "center",   
+                // backgroundColor: "#F4EBE6",  
+                // objectFit: "contain",
+                // boxSizing: "border-box",
              }}>  
+             {/* <p> HI </p> */}
                  <h1 className="About">{title}</h1>
-             <p style={{ margin: '5 30px' }} className="About2">{info}</p>
+             {/* <p style={{}} className="About">{title}</p>     */}
+             <p className="About2">{info}</p>
              </div>
     
   );
